bd1305066ced6425e3dc83885d873e3c6037108b
= <mukhtarrahimi.dev@gmail.com>
2025-10-04 10:18:15 +0430

    navbar styel

--- components/navbar/Navbar.js
+++ components/navbar/Navbar.js
@@ -1,33 +1,46 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Link from 'next/link';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faBars } from '@fortawesome/free-solid-svg-icons';
+import { faBars, faTimes } from '@fortawesome/free-solid-svg-icons';
 
 function Navbar() {
+  const [isClick, setIsClick] = useState(false);
+  const toggleBtn = () => {
+    setIsClick(!isClick);
+  };
+
   return (
     <div>
-      <div className="navbar h-screen w-24  bg-yellow-500 flex items-start justify-center">
-        <button className="mt-6 p-1 border-2 rounded-lg border-gray-900 ">
-          <FontAwesomeIcon icon={faBars} className="text-gray-900 text-xl" />
+      {/* Navbar عمودی */}
+      <div className="navbar h-screen w-24 bg-yellow-500 flex items-start justify-center fixed top-0 right-0 z-50">
+        <button
+          onClick={toggleBtn}
+          className="mt-6 p-2 border-2 rounded-lg border-gray-900 hover:bg-yellow-600 transition"
+        >
+          <FontAwesomeIcon
+            icon={isClick ? faTimes : faBars}
+            className="text-gray-900 text-2xl"
+          />
         </button>
       </div>
-      <div className="w-screen h-screen flex flex-col justify-center items-center gap-10 bg-gray-900">
-        <ul>
-          <li className="bg-yellow-500 w-36 h-10 flex items-center justify-center hover:bg-yellow-600 duration-500 rounded-lg text-white">
-            <Link href="/">صفحه اصلی</Link>
-          </li>
-          <li className="bg-yellow-500 w-36 h-10 flex items-center justify-center hover:bg-yellow-600 duration-500 rounded-lg text-white">
-            <Link href="/">خانه ها</Link>
-          </li>
-          <li className="bg-yellow-500 w-36 h-10 flex items-center justify-center hover:bg-yellow-600 duration-500 rounded-lg text-white">
-            <Link href="/">درباره ما</Link>
-          </li>
-          <li className="bg-yellow-500 w-36 h-10 flex items-center justify-center hover:bg-yellow-600 duration-500 rounded-lg text-white">
-            <Link href="/">تماس با ما</Link>
-          </li>
-          <li className="bg-yellow-500 w-36 h-10 flex items-center justify-center hover:bg-yellow-600 duration-500 rounded-lg text-white">
-            <Link href="/">گالری</Link>
-          </li>
+
+      {/* منوی Overlay */}
+      <div
+        className={`fixed top-0 right-24 w-screen h-screen bg-gray-900 bg-opacity-70 backdrop-blur-sm flex flex-col justify-center items-center gap-6 transition-all duration-500 ${
+          isClick ? 'opacity-100 visible' : 'opacity-0 invisible'
+        }`}
+      >
+        <ul className="flex flex-col justify-center items-center gap-6">
+          {['صفحه اصلی', 'خانه ها', 'درباره ما', 'تماس با ما', 'گالری'].map(
+            (item) => (
+              <li
+                key={item}
+                className="bg-yellow-500 w-40 h-12 flex items-center justify-center hover:bg-yellow-600 duration-300 rounded-lg text-white text-lg font-heading shadow-lg"
+              >
+                <Link href="/">{item}</Link>
+              </li>
+            )
+          )}
         </ul>
       </div>
     </div>
