bf1c7b4ed271e770492e7bb738a63ba52017b870
= <mukhtarrahimi.dev@gmail.com>
2025-10-05 20:01:56 +0430

    feature info

--- components/templates/index/Features.js
+++ components/templates/index/Features.js
@@ -1,12 +1,62 @@
 import React from 'react';
 //  font awesome
-import '@/styles/globals.css';
-import { config } from '@fortawesome/fontawesome-svg-core';
-import '@fortawesome/fontawesome-svg-core/styles.css';
-config.autoAddCss = false;
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {
+  faBarChart,
+  faGlobe,
+  faKey,
+  faLock,
+  faMapMarker,
+  faTrophy,
+} from '@fortawesome/free-solid-svg-icons';
 
 function Features() {
-  return <div className="features">[{'icon': ''}]</div>;
+  return (
+    <div className="features">
+      <div className="feature">
+        {[
+          {
+            icon: 'faGlobe',
+            title: 'بهترین خانه های لوکس جهان',
+            info: '',
+          },
+          {
+            icon: 'faMapMarker',
+            title: 'همه خانه ها در مکان های برتر',
+            info: '',
+          },
+          {
+            icon: 'faKey',
+            title: 'خانه جدید در یک هفته',
+            info: '',
+          },
+          {
+            icon: 'faLock',
+            title: 'پرداخت های ایمن در بعدی',
+            info: '',
+          },
+          {
+            icon: 'faTrophy',
+            title: 'فقط بهترین خواص',
+            info: '',
+          },
+          {
+            icon: 'faMapMarker',
+            title: '1% مشاوران املاک برتر',
+            info: '',
+          },
+        ].map((fea) => (
+          <li key={fea.title}>
+            <FontAwesomeIcon icon={fea.icon} />
+            <div>
+              <h2>{fea.title}</h2>
+              <p>{fea.info}</p>
+            </div>
+          </li>
+        ))}
+      </div>
+    </div>
+  );
 }
 
 export default Features;
