e92dc4418052f3d6d46024a908fa2eda0f0cc489
= <mukhtarrahimi.dev@gmail.com>
2025-10-05 19:35:41 +0430

    font-awesome add to features file

--- components/templates/index/Features.js
+++ components/templates/index/Features.js
@@ -1,7 +1,12 @@
 import React from 'react';
+//  font awesome
+import '@/styles/globals.css';
+import { config } from '@fortawesome/fontawesome-svg-core';
+import '@fortawesome/fontawesome-svg-core/styles.css';
+config.autoAddCss = false;
 
 function Features() {
-  return <div></div>;
+  return <div className="features">[{'icon': ''}]</div>;
 }
 
 export default Features;
