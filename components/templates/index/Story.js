865e2cb75c94a14d0d051574b587dbe618791e22
= <mukhtarrahimi.dev@gmail.com>
2025-10-05 21:13:47 +0430

    img add to story

--- components/templates/index/Story.js
+++ components/templates/index/Story.js
@@ -1,7 +1,17 @@
 import React from 'react';
+import Image from 'next/image';
 
 function Story() {
-  return <div>Story</div>;
+  return (
+    <div className="story">
+      <div className="right">
+        <Image src="/img/back.jpg" />
+        <Image src="/img/story-1.jpeg" />
+        <Image src="/img/story-2.jpeg" />
+      </div>
+      <div className="left"></div>
+    </div>
+  );
 }
 
 export default Story;
--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,12 @@
 import Features from '@/components/templates/index/Features';
+import Story from '@/components/templates/index/Story';
 import React from 'react';
 
 function index() {
   return (
     <div>
       <Features />
+      <Story />
     </div>
   );
 }
