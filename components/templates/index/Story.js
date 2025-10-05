cc3aee421b9515327be487f7a113d6ade940c3dc
= <mukhtarrahimi.dev@gmail.com>
2025-10-05 21:17:29 +0430

    img

--- components/templates/index/Story.js
+++ components/templates/index/Story.js
@@ -5,9 +5,9 @@ function Story() {
   return (
     <div className="story">
       <div className="right">
-        <Image src="/img/back.jpg" />
-        <Image src="/img/story-1.jpeg" />
-        <Image src="/img/story-2.jpeg" />
+        <Image src="/img/back.jpg" width={200} height={200} />
+        <Image src="/img/story-1.jpeg" width={200} height={200} />
+        <Image src="/img/story-2.jpeg" width={200} height={200} />
       </div>
       <div className="left"></div>
     </div>
