0e3b8490e4965320e48e1880f9fc06c3297058b0
= <mukhtarrahimi.dev@gmail.com>
2025-10-06 21:32:28 +0430

    story left

--- components/templates/index/Story.js
+++ components/templates/index/Story.js
@@ -1,15 +1,43 @@
 import React from 'react';
 import Image from 'next/image';
+import Link from 'next/link';
 
 function Story() {
   return (
-    <div className="story">
-      <div className="right">
-        <Image src="/img/back.jpg" width={200} height={200} />
-        <Image src="/img/story-1.jpeg" width={200} height={200} />
-        <Image src="/img/story-2.jpeg" width={200} height={200} />
+    <div className="story my-8">
+      <div className="relative right w-full md:w-[60%]">
+        <Image
+          src="/img/back.jpg"
+          width={200}
+          height={200}
+          className=" w-full "
+        />
+        <Image
+          src="/img/story-1.jpeg"
+          width={300}
+          height={300}
+          className="absolute top-12 right-20"
+        />
+        <Image
+          src="/img/story-2.jpeg"
+          width={200}
+          height={200}
+          className="absolute top-3 right-6"
+        />
       </div>
-      <div className="left"></div>
+      <div className="left w-full md:w-[40%]">
+        <p>مشتریان خوشحال</p>
+        <h2>”بهترین تصمیم زندگی ما“</h2>
+        <p>
+          متنی ساختگی و بدون معنی است که برای امتحان فونت و یا پر کردن فضا در یک
+          طراحی گرافیکی و یا صنعت چاپ استفاده میشود. طراحان وب و گرافیک از این
+          متن برای پرکردن
+        </p>
+        <button>
+          <Link href="/" />
+          خانه خود را پیدا کنید
+        </button>
+      </div>
     </div>
   );
 }
