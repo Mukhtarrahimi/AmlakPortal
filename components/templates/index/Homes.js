e7b260dc2cf71d4ee11137ed270181fb9871a4ca
= <mukhtarrahimi.dev@gmail.com>
2025-10-06 22:32:12 +0430

    homes

--- components/templates/index/Homes.js
+++ components/templates/index/Homes.js
@@ -1,12 +1,24 @@
 import React from 'react';
 import db from './../../../data/db.json';
 import Image from 'next/image';
+import Link from 'next/link';
 function Homes() {
   return (
     <div>
-      {db.map((home) => (
+      {db.homes.map((home) => (
         <div className="home">
-          <Image />
+          <Image src={home.img} width={400} height={400} />
+          <h2>{home.title}</h2>
+          <div>
+            <p>{home.meterage}میتراژ</p>
+            <p>{home.roomCount}تعداد اطاق ها</p>
+          </div>
+          <div>
+            <p>قیمت {home.prce}</p>
+          </div>
+          <button>
+            <Link href={`/`}></Link>
+          </button>
         </div>
       ))}
     </div>
