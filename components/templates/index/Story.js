db29835ad5c5aa1165e3b33a70017504e102be89
= <mukhtarrahimi.dev@gmail.com>
2025-10-06 21:54:11 +0430

    style complate

--- components/templates/index/Story.js
+++ components/templates/index/Story.js
@@ -4,7 +4,7 @@ import Link from 'next/link';
 
 function Story() {
   return (
-    <div className="story my-8 md:flex gap-10 items-center justify-center bg-gray-100">
+    <div className="story my-8 md:flex gap-12 items-center justify-center bg-gray-100">
       <div className="relative right w-full md:w-[60%]">
         <Image
           src="/img/back.jpg"
