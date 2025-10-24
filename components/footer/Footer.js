2ef7ff6933bdc39b7db80d455fe070c6c4482ed8
= <mukhtarrahimi.dev@gmail.com>
2025-10-24 15:11:59 +0430

    style update

--- components/footer/Footer.js
+++ components/footer/Footer.js
@@ -3,7 +3,7 @@ import React from 'react';
 function Footer() {
   return (
     <footer className="footer bg-gray-900 p-4 text-white">
-      <ul className="nav">
+      <ul className="nav grid grid-cols-2 md:grid-cols-6 gap-6 ">
         <li className="nav__item">
           <a href="#" className="nav__link">
             خانه رویایی خود را پیدا کنید
@@ -36,7 +36,7 @@ function Footer() {
         </li>
       </ul>
 
-      <p className="copyright">
+      <p className="copyright pt-8 text-center">
         &copy; حقوق مادی معنوی این سایت برای
         <strong className="copyright__name">مهدی ایلخانی نسب</strong>
         محفوظ میباشد
